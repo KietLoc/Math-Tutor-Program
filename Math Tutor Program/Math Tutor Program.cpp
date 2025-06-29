b11dcc737fc16f752ad44f3ed6c52b83ffef7d7e
KietLoc <ktl20634@email.vccs.edu>
2025-06-29 19:15:34 -0400

    Add project files.

---
+++ Math Tutor Program/Math Tutor Program.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+double a, b, c;
+char d;
+
+static double calcAdd(double a, double b)
+{
+    return a + b;
+}
+
+int main()
+{
+	cout << "Enter the first number of your addition: " << endl;
+	cin >> a;
+	cout << "Enter the second number of your addition: " << endl;
+	cin >> b;
+
+    double c = calcAdd(a, b);
+
+    cout << "Press 'P' and hit Enter to see the result: " << endl;
+
+    //Ignore any leftover newline from previous input
+    cin.ignore();
+    
+    //Wait until the user enters 'P' or 'p'
+    while (true)
+    {
+        d = cin.get();
+        if (d == 'P' || d == 'p')
+            break;
+    }
+
+    cout << "The answer is: " << a << " + " << b << " = " << c << endl;
+
+    return 0;
+}
